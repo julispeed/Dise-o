4b76596fc1246c06f9e278faa99fa79b1a476103
julispeed <julian.acuna2003@gmail.com>
2024-04-25 00:06:51 -0300

    tercero

---
+++ Fecha.cs
@@ -0,0 +1,293 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace ProyectoFecha
+{
+
+    public class Fecha 
+    {
+
+        #region Atributos estaticos
+        //DECLARACION DE ATRIBUTOS ESTATICOS
+        private static int dia_inicial = 1;
+        private static int mes_inicial = 1;
+        private static int ano_inicial = 2000;
+        #endregion
+
+        #region Atributos
+        //Declaracion de Atributos
+        private int _dia;
+        private int _mes;
+        private int _ano;
+        #endregion
+        #region Propiedades
+        public int D
+        {
+            get { return _dia; }
+            set {
+                this._dia = EsFechaValida(value, mes_inicial, ano_inicial) ? value : _dia;
+            }
+        }        
+        public int M
+        {
+            get { return _mes; }
+            set {
+                this._mes = EsFechaValida(dia_inicial, value, ano_inicial) ? value : _mes;
+                }
+        }
+        public int A
+        {
+            get { return _ano; }
+            set
+            {
+                this._ano=EsFechaValida(dia_inicial, mes_inicial, value) ? value : _ano;
+            }
+        }
+        #endregion
+        #region Constructor
+        public Fecha (int d,int m , int a)
+        {
+            if ( EsFechaValida(d,m,a))
+            {
+                D = d;
+                M = m;
+                A = a;
+            }
+            else
+            {
+                D = dia_inicial;
+                M = mes_inicial;
+                A = ano_inicial;
+            }
+        }
+        public Fecha()
+        {
+            D= dia_inicial;
+            M= mes_inicial;
+            A= ano_inicial;
+        }
+        #endregion
+        #region Comandos
+        public void IncrementarDia()
+        {
+            if (D == UltimoDiaDelMes(M,A))
+            {
+                D = 1;
+                if(M==12)
+                {
+                    A++;
+                    M = 1;
+                }
+                else
+                {
+                    M++;
+                }
+            }
+            else
+            {
+                D++;
+            }
+        }
+        public void DecrementarDia()
+        {
+            //if (D == UltimoDiaDelMes(M, A))
+            //{
+            //    D = 1;
+            //    if (M == 12)
+            //    {
+            //        A++;
+            //        M = 1;
+            //    }
+            //    else
+            //    {
+            //        M++;
+            //    }
+            //}
+            //else
+            //{
+            //    D++;
+            //}
+        }
+        public static int UltimoDiaDelMes(int mes, int ano)
+        {
+            int ultimodia = 0;
+            if (mes==1 || mes==3 || mes == 5 || mes==7 || mes == 8 || mes == 10 || mes ==12)
+            {
+                ultimodia = 31;
+            }
+            else if (mes == 4 || mes == 6 || mes == 9 || mes ==11)
+            {
+                ultimodia = 30;
+            }
+            else if (mes==2 && Esbisiesto(ano))
+            {
+                ultimodia = 29;
+            }
+            else if (mes==2 && !Esbisiesto(ano))
+            {
+                ultimodia = 28;
+            }
+            return  ultimodia;
+        }
+        #endregion
+
+        #region Consultas
+
+        public static bool EsFechaValida(int d,int m, int a)
+        {
+            bool resultado = true;            
+                switch (m)
+                {
+                    case 1://enero
+                        if (d<1 | d>31)
+                    {
+                        resultado = false;
+                    }
+                        break;
+                    case 3:// Marzo
+                        if (d <=0 || d > 31)
+                        { resultado = false; }
+                        break;
+                    case 5:  // Mayo
+                        if (d <=0 || d > 31)
+                        { resultado = false; }
+                        break;
+                    case 7:  // Julio
+                        if (d <=0 || d > 31)
+                        { resultado = false; }
+                        break;
+                    case 8:  // Agosto
+                        if (d <=0 || d > 31)
+                        { resultado = false; }
+                        break;
+                    case 10: // Octubre
+                        if (d <=0 || d > 31)
+                        { resultado = false; }
+                        break;
+                    case 12: // Diciembre
+                        if (d <=0 || d > 31)
+                        { resultado = false; }
+                        break;
+                    case 4:  // Abril
+                        if (d <=0 & d >= 31)
+                        { resultado = false; }
+                        break;
+                    case 6:  // Junio
+                        if (d <=0 & d >= 31)
+                        { resultado = false; }
+                        break;
+                    case 9:  // Septiembre
+                        if (d <=0 & d >= 31)
+                        { resultado = false; }
+                        break;
+                    case 11: // Noviembre
+                        if (d <=0 & d >= 31)
+                        { resultado = false; }
+                        break;
+                    case 2:  // Febrero
+                        if (Esbisiesto(a))
+                        {
+                            if (d <=0 || d > 28)
+                                resultado = false;
+
+                        }
+                        else
+                        {
+                            if (d <=0 || d > 29)
+                                resultado = false;
+                        }
+                        break;
+                    default:
+                    if (m < 1 || m > 12)
+                        resultado = false;
+                    break;
+                }                   
+            return resultado;            
+        }
+
+        public static bool Esbisiesto(int a)
+        {
+            bool resultado = false;
+            if ((a % 4 == 0 && a % 100 != 0) || (a % 400 == 0))
+                resultado = true;
+
+           return resultado;
+        }
+
+        public bool EsIgualA(Fecha OtraFecha)
+        {
+            return ((OtraFecha.D == this.D) && (OtraFecha.M == this.M) && (OtraFecha.A == this.A));                        
+        }
+
+        public bool MayorQue(Fecha OtraFecha)
+        {
+            bool respuesta = false;
+            if (OtraFecha.A>this.A)
+            {
+                respuesta = true;
+            }    
+            else if (OtraFecha.A==this.A && OtraFecha.M>this.M)
+            {
+                respuesta = true;
+            }
+            else if (OtraFecha.A == this.A && OtraFecha.M==this.M && OtraFecha.D>this.D)
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+
+        public bool MenorQue(Fecha OtraFecha)
+        {
+            bool respuesta = false;
+            if (OtraFecha.A < this.A)
+            {
+                respuesta = true;
+            }
+            else if (OtraFecha.A == this.A && OtraFecha.M < this.M)
+            {
+                respuesta = true;
+            }
+            else if (OtraFecha.A == this.A && OtraFecha.M == this.M && OtraFecha.D < this.D)
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+
+        public int DiferenciaEntreFechas(Fecha OtraFecha)
+        {
+            int cantDias = 0;           
+          if (this.MayorQue(OtraFecha))
+            {
+                
+                Fecha Aux = new Fecha(this.D, this.M, this.A);
+                while (!Aux.EsIgualA(OtraFecha))
+                {
+                    Aux.IncrementarDia();
+                    cantDias++;
+                }
+            }
+          else
+            {
+                while (!OtraFecha.EsIgualA(this))
+                {
+                    OtraFecha.IncrementarDia();
+                    cantDias++;
+                }
+            }
+          
+            return cantDias;
+        }
+        
+        #endregion
+    }
+}
---
+++ InterfazFecha.cs
@@ -0,0 +1,197 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProyectoFecha
+{
+    public partial class InterfazFecha : Form
+    {
+        public InterfazFecha()
+        {
+            InitializeComponent();
+        }
+
+        private Fecha[] ListaFechas = new Fecha[maximo];
+        private static int maximo=50;
+        private int cantidad = 0;
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BAgregar_Click(object sender, EventArgs e)
+        {
+            string[] dato = MTFechainicial.Text.Replace(" ", "").Split('/');            
+            if (dato[0] == "" && dato[1] == "" && dato[2] == "")
+            {
+                MessageBox.Show("Parametros invalidos, Ingresa automaticamente la fecha predeterminada");
+                InsertarOrdenado(new Fecha());
+                listarfechas();
+                return;
+            }
+            else if (dato[0] == "" || dato[1] == "" || dato[2] == "")
+            {
+                MessageBox.Show("Parametros invalidos, no se agrega la fecha");
+                return;
+            }
+            InsertarOrdenado(new Fecha(int.Parse(dato[0]), int.Parse(dato[1]), int.Parse(dato[2]))); 
+            listarfechas();            
+        }
+        public void InsertarOrdenado(Fecha f)
+        {
+            if (cantidad == 0)
+            {
+                ListaFechas[cantidad] = f;
+            }
+            else
+            {
+                for (int i = cantidad-1; i >= 0; i--)
+                {
+                    if (ListaFechas[i].A > f.A)
+                    {
+                        ListaFechas[i + 1] = ListaFechas[i];
+                        ListaFechas[i] = f;
+                    }
+                    else if (ListaFechas[i].A == f.A)
+                    {
+                        if (ListaFechas[i].M > f.M)
+                        {
+                            ListaFechas[i + 1] = ListaFechas[i];
+                            ListaFechas[i] = f;
+                        }
+                        else if (ListaFechas[i].D > f.D)
+                        {
+                            ListaFechas[i + 1] = ListaFechas[i];
+                            ListaFechas[i] = f;
+                        }
+                        else
+                        {
+                            ListaFechas[i + 1] = f;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        ListaFechas[i + 1] = f;
+                        break;
+                    }
+                }
+            }
+            cantidad++;
+        }
+        public void listarfechas()
+        {
+            LBFechas.Items.Clear();
+            foreach (var fec in ListaFechas)
+            {
+                if (fec!=null)
+                {
+                    LBFechas.Items.Add(" Dia :" + fec.D + " mes: " + fec.M + " año: " + fec.A);
+                }
+            }
+        }
+
+        private void BDiasEntre_Click(object sender, EventArgs e)
+        {
+            if (LBFechas.SelectedIndex != -1)
+            {
+                string[] dato = MTFechasentre.Text.Replace(" ", "").Split('/');
+                if (dato[0] == "" && dato[1] == "" && dato[2] == "")
+                {
+                    MessageBox.Show("Fecha Inválida, no se puede calcular.");
+                }
+                else
+                {
+                    MessageBox.Show("La diferencia entre las fechas es: " + ListaFechas[LBFechas.SelectedIndex].DiferenciaEntreFechas(new Fecha (Convert.ToInt32(dato[0]), Convert.ToInt32(dato[1]), Convert.ToInt32(dato[2]))));
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar una fecha de la lista.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void Bigual_Click(object sender, EventArgs e)
+        {
+            if (LBFechas.SelectedIndex != -1)
+            {
+                string[] dato = MTComparacion.Text.Replace(" ", "").Split('/');
+                if (dato[0] == "" && dato[1] == "" && dato[2] == "")
+                {
+                    MessageBox.Show("Fecha Inválida, no se puede comparar.");
+                }
+                else if (ListaFechas[LBFechas.SelectedIndex].EsIgualA(new Fecha(Convert.ToInt32(dato[0]), Convert.ToInt32(dato[1]), Convert.ToInt32(dato[2]))))
+                {
+                    MessageBox.Show("Las Fechas son iguales", "Consulta 'Igual'", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Las fechas no son iguales");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar una fecha de la lista.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void InterfazFecha_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BMayorque_Click(object sender, EventArgs e)
+        {
+            if (LBFechas.SelectedIndex != -1)
+            {
+                string[] dato = MTComparacion.Text.Replace(" ", "").Split('/');
+                if (dato[0] == "" && dato[1] == "" && dato[2] == "")
+                {
+                    MessageBox.Show("Fecha Inválida, no se puede comparar.");
+                }
+                else if (ListaFechas[LBFechas.SelectedIndex].MayorQue(new Fecha(Convert.ToInt32(dato[0]), Convert.ToInt32(dato[1]), Convert.ToInt32(dato[2]))))
+                {
+                    MessageBox.Show("La Fecha es mayor que la seleccionada", "Consulta 'Mayor Que'", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Las fecha no es Mayor");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar una fecha de la lista.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void BMenorQue_Click(object sender, EventArgs e)
+        {
+            if (LBFechas.SelectedIndex != -1)
+            {
+                string[] dato = MTComparacion.Text.Replace(" ", "").Split('/');
+                if (dato[0] == "" && dato[1] == "" && dato[2] == "")
+                {
+                    MessageBox.Show("Fecha Inválida, no se puede comparar.");
+                }
+                else if (ListaFechas[LBFechas.SelectedIndex].MenorQue(new Fecha(Convert.ToInt32(dato[0]), Convert.ToInt32(dato[1]), Convert.ToInt32(dato[2]))))
+                {
+                    MessageBox.Show("La Fecha es menor que la seleccionada", "Consulta 'Mayor Que'", MessageBoxButtons.OK);
+                }
+                else
+                {
+                    MessageBox.Show("Las fecha no es Mayor");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Debe seleccionar una fecha de la lista.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
